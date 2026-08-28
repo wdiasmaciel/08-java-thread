c1b1292b84ac610ae7b5004c54e9059064929c0f
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2026-08-28 21:10:34 +0000

    Exemplo

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Estacionamento estacionamento = new Estacionamento();
 
-        // Criamos 8 carros (threads) para disputar as 3 vagas:
+        // Criamos 8 carros (threads) para disputar 3 vagas no estacionamento:
         for (int i = 1; i <= 8; i++) {
             String nomeCarro = "Carro_" + i;
             new Thread(() -> estacionamento.entrar(nomeCarro)).start();
